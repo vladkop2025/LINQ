efbe97a52d27431996f6975469233ba555d8b2de
vladkop2025 <vladkop@mail.ru>
2025-04-18 12:31:49 +0300

    linq_07

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -28,16 +28,29 @@ namespace LinqTest
             List<Student> students = new List<Student>
             {
                 new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
-                 new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
+                new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
                 new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }},
                 new Student {Name="Василий", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
             };
 
-            var names = from s in students select s.Name;
+            var names = from s in students select s.Name; //В выборке у нас — коллекция строк
 
             // Выведем результат
             foreach (var name in names)
                 Console.WriteLine(name);
+
+            //Андрей
+            //Сергей
+            //Дмитрий
+            //Василий
+
+        }
+        // Создадим модель Student
+        class Student
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public List<string> Languages { get; set; }
         }
     }
 }
