828bc33ab8adc63d5148cc9ff8e053b140257c07
vladkop2025 <vladkop@mail.ru>
2025-04-18 12:06:40 +0300

    linq_04

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -10,9 +10,43 @@ namespace LinqTest
         static void Main(string[] args)
         {
             /*
-            Введение - удобный инструмент — LINQ, или Language Integrated Query
+            2/6   14.1. Фильтрация Where() - WhereExample
             */
+           
+            // Добавим Россию с её городами
+           var russianCities = new List<City>();
+           russianCities.Add(new City("Москва", 11900000));
+           russianCities.Add(new City("Санкт-Петербург", 4991000));
+           russianCities.Add(new City("Волгоград", 1099000));
+           russianCities.Add(new City("Казань", 1169000));
+           russianCities.Add(new City("Севастополь", 449138));
 
+            //Выберем города-миллионники:
+            var bigCities = from russianCity in russianCities
+                            where russianCity.Population > 1000000
+                            orderby russianCity.Population descending
+                            select russianCity;
+
+            foreach (var bigCity in bigCities)
+                Console.WriteLine(bigCity.Name + " - " + bigCity.Population);
+
+            //Москва - 11900000
+            //Санкт - Петербург - 4991000
+            //Казань - 1169000
+            //Волгоград - 1099000
+        }
+
+        // Создадим модель класс для города
+        public class City
+       {
+           public City(string name, long population)
+           {
+               Name = name;
+               Population = population;
+           }
+          
+           public string Name { get; set; }
+           public long Population { get; set; }
 
         }
     }
